d7be1ab10e1374c8559c4c3e157897ed705910df
Hizocar <hizocar@gmail.com>
2026-01-29 14:47:01 -0300

    improvement

--- src/app/auth/callback/route.ts
+++ src/app/auth/callback/route.ts
@@ -4,13 +4,27 @@ import { createClient } from '@/lib/supabase/server'
 export async function GET(request: Request) {
     const { searchParams, origin } = new URL(request.url)
     const code = searchParams.get('code')
+    // if "next" is in param, use it, otherwise default to /app
     const next = searchParams.get('next') ?? '/app'
 
     if (code) {
         const supabase = await createClient()
         const { error } = await supabase.auth.exchangeCodeForSession(code)
         if (!error) {
-            return NextResponse.redirect(`${origin}${next}`)
+            const isLocalEnv = process.env.NODE_ENV === 'development'
+            if (isLocalEnv) {
+                return NextResponse.redirect(`${origin}${next}`)
+            } else {
+                const forwardedHost = request.headers.get('x-forwarded-host') // original origin before load balancer
+                const isForwardedProtoHttps = request.headers.get('x-forwarded-proto') === 'https'
+
+                if (forwardedHost) {
+                    const protocol = isForwardedProtoHttps ? 'https' : 'http'
+                    return NextResponse.redirect(`${protocol}://${forwardedHost}${next}`)
+                } else {
+                    return NextResponse.redirect(`${origin}${next}`)
+                }
+            }
         }
     }
 
